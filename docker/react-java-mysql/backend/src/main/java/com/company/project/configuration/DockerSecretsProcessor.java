22266b7792adaaa61796c6bd84a5e0e68e32f926
Gupta, Piyush | Piyush | CPD <piyush.gupta@rakuten.com>
2021-06-26 16:21:37 +0900

    Initial Files

---
+++ docker/react-java-mysql/backend/src/main/java/com/company/project/configuration/DockerSecretsProcessor.java
@@ -0,0 +1,32 @@
+package com.company.project.configuration;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.env.EnvironmentPostProcessor;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.util.StreamUtils;
+
+/**
+ * Read property from docker secret file.
+ */
+public class DockerSecretsProcessor implements EnvironmentPostProcessor {
+
+	@Override
+	public void postProcessEnvironment(ConfigurableEnvironment environment, SpringApplication application) {
+		//Resource resource = new FileSystemResource("/run/secrets/db-password");
+		//if (resource.exists() && System.getProperty("MYSQL_PASSWORD") == null) {
+		  if (System.getProperty("MYSQL_PASSWORD") != null) {
+			try {
+				//String dbPassword = StreamUtils.copyToString(resource.getInputStream(), Charset.defaultCharset());
+				String dbPassword =  System.getProperty("MYSQL_PASSWORD"); 
+				System.setProperty("MYSQL_PASSWORD", dbPassword);
+			} catch (Exception e) {
+				throw new RuntimeException(e);
+			}
+		}
+	}
+}
